71c6842c5c3add53b3894761df71f3f4b1a93d4e
Gustavo Silva Leite Lopes <72940818+Gustavolopesrj@users.noreply.github.com>
2021-02-17 09:05:09 -0300

    Add files via upload

---
+++ Projeto React Native/src/Components/ApiProdutos.js
@@ -0,0 +1,46 @@
+
+
+const ApiProdutos = [
+    {
+        id: Math.random(),
+        produto: 'The Witcher 3 - Wild Hunt',
+        descricao: 'RPG',
+        preco: 150.00,
+        precoAnterior: 200.00,
+        imagem: 'thewitcher3.jpeg'
+    },
+    {
+        id: Math.random(),
+        produto: 'Wolfenstein 2 - The New Colossus',
+        descricao: 'Tiro',
+        preco: 50.00,
+        precoAnterior: 100.00,
+        imagem: 'wolfenstein.jpg'
+    },
+    {
+        id: Math.random(),
+        produto: 'Gran Turismo Sport',
+        descricao: 'Corrida',
+        preco: 90.00,
+        precoAnterior: 150.00,
+        imagem: 'granturismo.jpg'
+    },
+    {
+        id: Math.random(),
+        produto: 'God of War',
+        descricao: 'Ação',
+        preco: 150.00,
+        precoAnterior: 200.00,
+        imagem: 'gow.jpg'
+    },
+    {
+        id: Math.random(),
+        produto: 'Samurai Shodown',
+        descricao: 'Luta',
+        preco: 150.00,
+        precoAnterior: 200.00,
+        imagem: 'samurai.jpg'
+    }
+
+]
+export default ApiProdutos;
